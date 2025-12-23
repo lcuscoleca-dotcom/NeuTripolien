c0c76551165ac92141b1dca99fbeeba37790aa76
Sirtaj-kang <sirtaj.kang@stud.hcw.ac.at>
2025-12-23 16:22:50 +0100

    Neu Tripolien_20/12/2025 3. version

--- src/main/java/neu/tripolien/neutripolien/HelloApplication.java
+++ src/main/java/neu/tripolien/neutripolien/HelloApplication.java
@@ -19,9 +19,21 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("PlayScreen.fxml"));
         Scene scene2 = new Scene(fxmlLoader2.load(), 320, 240);
 
+        // Dritte Scene (DeathScreen)
+        FXMLLoader fxmlLoader3 = new FXMLLoader(HelloApplication.class.getResource("DeathScreen.fxml"));
+        Scene scene3 = new Scene(fxmlLoader3.load(), 320, 240);
+
+        // Vierte Scene (WinScreen)
+        FXMLLoader fxmlLoader4 = new FXMLLoader(HelloApplication.class.getResource("WinScreen.fxml"));
+        Scene scene4 = new Scene(fxmlLoader4.load(), 320, 240);
+
+        // Fünfte Scene (MainMenuScreen)
+        FXMLLoader fxmlLoader5 = new FXMLLoader(HelloApplication.class.getResource("MainMenuScreen.fxml"));
+        Scene scene5 = new Scene(fxmlLoader5.load(), 320, 240);
+
         // Stage initial auf Scene 1 setzen
         stage.setTitle("Neu Tripolien!");
-        stage.setScene(scene2);
+        stage.setScene(scene5);
         stage.show();
 
     }
---
+++ src/main/java/neu/tripolien/neutripolien/WinscreenController.java
@@ -0,0 +1,4 @@
+package neu.tripolien.neutripolien;
+
+public class WinscreenController {
+}
